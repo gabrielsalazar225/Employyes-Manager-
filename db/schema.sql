f0fd9929a06c43aedd2895b3b07360b5f41007e4
gabrielsalazar225 <165944712+gabrielsalazar225@users.noreply.github.com>
2024-08-11 23:36:30 -0400

    all good

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,23 @@
+DROP DATABASE IF EXISTS employees_db;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+
 CREATE DATABASE employees_db;
-
-CREATE TABLE department (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+CREATE TABLE departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
-
-CREATE TABLE role (
-  id SERIAL PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INTEGER NOT NULL REFERENCES department(id)
+CREATE TABLE roles (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INTEGER REFERENCES departments(id)
 );
-
-CREATE TABLE employee (
-  id SERIAL PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL REFERENCES role(id),
-  manager_id INTEGER REFERENCES employee(id)
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER REFERENCES roles(id),
+    manager_id INTEGER REFERENCES employees(id)
 );
